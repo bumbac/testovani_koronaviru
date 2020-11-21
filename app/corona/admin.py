9389cb9ec5b84e5ca5f933f52819a047640ae404
minarsim <minarsim@fit.cvut.cz>
2020-11-21 17:18:03 +0100

    Modify admin panel

--- app/corona/admin.py
+++ app/corona/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Patient)
+admin.site.register(HygienicStation)
+admin.site.register(Facility)
+admin.site.register(Place)
+admin.site.register(Quarantine)
+admin.site.register(Reservation)
+admin.site.register(CovidTest)
+admin.site.register(Covidpass)
