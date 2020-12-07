075e3071398df482f43852eb00582e56a9cbfc1d
minarsim <minarsim@fit.cvut.cz>
2020-12-07 10:42:05 +0100

    Update use of factories in business logic layer

--- app/corona/business_layer/application_logic/patient_handler.py
+++ app/corona/business_layer/application_logic/patient_handler.py
@@ -1,15 +1,17 @@
 from corona.business_layer.application_logic.interfaces.patient_handler_interface import PatientHandlerInterface
-from corona.data_layer.patient_data_access import PatientDataAccess
+from corona.data_layer.interfaces.data_access_factory import DataAccessFactory
 
 """
 Implementation of PatientHandler interface
 """
 class PatientHandler(PatientHandlerInterface):
+    data_access_factory = DataAccessFactory()
+
     def create_patient(self, name, surname, birthid, email, phone, address, user):
-        patient_data_access = PatientDataAccess()
+        patient_data_access = self.data_access_factory.get_patient_data_access()
         patient_data_access.add_patient(name=name, surname=surname, birthid=birthid, email=email, phone=phone, address=address, user=user)
         return True
 
     def get_patient(self, user):
-        patient_data_access = PatientDataAccess()
+        patient_data_access = self.data_access_factory.get_patient_data_access()
         return patient_data_access.get_patient(user)
--- app/corona/business_layer/application_logic/reservation_handler.py
+++ app/corona/business_layer/application_logic/reservation_handler.py
@@ -1,5 +1,5 @@
 from corona.business_layer.application_logic.interfaces.reservation_handler_interface import ReservationHandlerInterface
-from corona.data_layer.reservation_data_access import ReservationDataAccess
+from corona.data_layer.interfaces.data_access_factory import DataAccessFactory
 from datetime import datetime
 
 
@@ -7,7 +7,9 @@ from datetime import datetime
 Implementation of ReservationHandler interface
 """
 class ReservationHandler(ReservationHandlerInterface):
+    data_access_factory = DataAccessFactory()
+
     def create_reservation(self, deadline, patient, covidpass):
-        reservation_data_access = ReservationDataAccess()
+        reservation_data_access = self.data_access_factory.get_reservation_data_access()
         reservation_data_access.add_reservation(createdate=datetime.now().strftime('%Y-%m-%d %H:%M'), deadline=deadline, patient=patient, covidpass=covidpass)
         return True
