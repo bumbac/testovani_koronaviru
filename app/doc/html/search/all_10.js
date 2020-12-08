1d1b9a54ba74d2259906397722bfc5e40f5da824
minarsim <minarsim@fit.cvut.cz>
2020-12-08 11:44:43 +0100

    Update documentation

--- app/doc/html/search/all_10.js
+++ app/doc/html/search/all_10.js
@@ -1,6 +1,5 @@
 var searchData=
 [
-  ['readme',['README',['../md___users_mnrk__documents__skola_5_8__semester__b_i-_s_i1_82_testovani_koronaviru_app__r_e_a_d_m_e.html',1,'']]],
   ['readme_2emd',['README.md',['../_r_e_a_d_m_e_8md.html',1,'']]],
   ['register_5fpatient_5fview_2epy',['register_patient_view.py',['../register__patient__view_8py.html',1,'']]],
   ['registerpatientview',['RegisterPatientView',['../classapp_1_1corona_1_1presentation__layer_1_1views_1_1register__patient__view_1_1_register_patient_view.html',1,'app::corona::presentation_layer::views::register_patient_view']]],
@@ -13,8 +12,8 @@ var searchData=
   ['reservationdataaccess',['ReservationDataAccess',['../classapp_1_1corona_1_1data__layer_1_1reservation__data__access_1_1_reservation_data_access.html',1,'app::corona::data_layer::reservation_data_access']]],
   ['reservationdataaccessinterface',['ReservationDataAccessInterface',['../classapp_1_1corona_1_1data__layer_1_1interfaces_1_1reservation__data__access__interface_1_1_reservation_data_access_interface.html',1,'app::corona::data_layer::interfaces::reservation_data_access_interface']]],
   ['reservationform',['ReservationForm',['../classapp_1_1corona_1_1forms_1_1_reservation_form.html',1,'app::corona::forms']]],
-  ['reservationhandler',['ReservationHandler',['../classapp_1_1corona_1_1business__layer_1_1application__logic_1_1reservation__handler_1_1_reservation_handler.html',1,'app::corona::business_layer::application_logic::reservation_handler']]],
-  ['reservationhandlerinterface',['ReservationHandlerInterface',['../classapp_1_1corona_1_1business__layer_1_1application__logic_1_1interfaces_1_1reservation__handle1474dd3dc24ef4b37de4ecf9c509f2ed.html',1,'app::corona::business_layer::application_logic::interfaces::reservation_handler_interface']]],
+  ['reservationhandler',['ReservationHandler',['../classapp_1_1corona_1_1business__layer_1_1handlers_1_1reservation__handler_1_1_reservation_handler.html',1,'app::corona::business_layer::handlers::reservation_handler']]],
+  ['reservationhandlerinterface',['ReservationHandlerInterface',['../classapp_1_1corona_1_1business__layer_1_1handlers_1_1interfaces_1_1reservation__handler__interfaedbb78edb575312e4f2e4ab7761d7a33.html',1,'app::corona::business_layer::handlers::interfaces::reservation_handler_interface']]],
   ['reservationview',['ReservationView',['../classapp_1_1corona_1_1presentation__layer_1_1views_1_1reservation__view_1_1_reservation_view.html',1,'app::corona::presentation_layer::views::reservation_view']]],
   ['root_5furlconf',['ROOT_URLCONF',['../namespaceapp_1_1app_1_1settings.html#aeea25bc27cd24a96741399e2068f3531',1,'app::app::settings']]]
 ];
--- app/doc/html/search/classes_9.js
+++ app/doc/html/search/classes_9.js
@@ -5,7 +5,7 @@ var searchData=
   ['reservationdataaccess',['ReservationDataAccess',['../classapp_1_1corona_1_1data__layer_1_1reservation__data__access_1_1_reservation_data_access.html',1,'app::corona::data_layer::reservation_data_access']]],
   ['reservationdataaccessinterface',['ReservationDataAccessInterface',['../classapp_1_1corona_1_1data__layer_1_1interfaces_1_1reservation__data__access__interface_1_1_reservation_data_access_interface.html',1,'app::corona::data_layer::interfaces::reservation_data_access_interface']]],
   ['reservationform',['ReservationForm',['../classapp_1_1corona_1_1forms_1_1_reservation_form.html',1,'app::corona::forms']]],
-  ['reservationhandler',['ReservationHandler',['../classapp_1_1corona_1_1business__layer_1_1application__logic_1_1reservation__handler_1_1_reservation_handler.html',1,'app::corona::business_layer::application_logic::reservation_handler']]],
-  ['reservationhandlerinterface',['ReservationHandlerInterface',['../classapp_1_1corona_1_1business__layer_1_1application__logic_1_1interfaces_1_1reservation__handle1474dd3dc24ef4b37de4ecf9c509f2ed.html',1,'app::corona::business_layer::application_logic::interfaces::reservation_handler_interface']]],
+  ['reservationhandler',['ReservationHandler',['../classapp_1_1corona_1_1business__layer_1_1handlers_1_1reservation__handler_1_1_reservation_handler.html',1,'app::corona::business_layer::handlers::reservation_handler']]],
+  ['reservationhandlerinterface',['ReservationHandlerInterface',['../classapp_1_1corona_1_1business__layer_1_1handlers_1_1interfaces_1_1reservation__handler__interfaedbb78edb575312e4f2e4ab7761d7a33.html',1,'app::corona::business_layer::handlers::interfaces::reservation_handler_interface']]],
   ['reservationview',['ReservationView',['../classapp_1_1corona_1_1presentation__layer_1_1views_1_1reservation__view_1_1_reservation_view.html',1,'app::corona::presentation_layer::views::reservation_view']]]
 ];
